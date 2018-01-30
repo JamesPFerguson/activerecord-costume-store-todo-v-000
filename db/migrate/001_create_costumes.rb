de89059be411de4c28c0748286d125c457285ca1
James Ferguson <ncjamesferguson@gmail.com>
2018-01-30 18:48:28 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -16,7 +16,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price
       t.string :size
       t.string :image_url
-      t.timestamps
+      t.timestamps :created_at
+      t.timestamps :updated_at
     end
   end
 
